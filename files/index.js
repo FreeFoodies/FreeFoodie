1cacf3067d1a656c2779aca790b5943fdfd823d5
Darryl Yeo <dev@darryl-yeo.com>
2020-01-11 14:15:30 -0800

    Fix merge duplicates

--- files/index.js
+++ files/index.js
@@ -1,17 +1,3 @@
-const firebaseConfig = {
-	apiKey: "AIzaSyAOI-pfdx43XfrMk9fBk0vfYH9kQzSOzP0",
-	authDomain: "food-delivery-for-the-homeless.firebaseapp.com",
-	databaseURL: "https://food-delivery-for-the-homeless.firebaseio.com",
-	projectId: "food-delivery-for-the-homeless",
-	storageBucket: "food-delivery-for-the-homeless.appspot.com",
-	messagingSenderId: "1048473239710",
-	appId: "1:1048473239710:web:5723f204a0e5a2fef27b8f",
-	measurementId: "G-BWWYR6Y856"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-const db = firebase.firestore();
 function whenGoogleMapsAPIReady() {
 	const map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: 39.8283, lng: -98.5795},
