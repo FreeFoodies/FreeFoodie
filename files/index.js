28b071b6c0f57607954eae8636259310df574449
Darryl Yeo <dev@darryl-yeo.com>
2020-01-11 14:14:40 -0800

    Refactor

--- files/index.js
+++ files/index.js
@@ -75,22 +75,33 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 
 
 // Firebase
-$(document).ready(() => {
-	// jQuery methods go here...
-	// Your web app's Firebase configuration
+// jQuery methods go here...
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyAOI-pfdx43XfrMk9fBk0vfYH9kQzSOzP0",
+	authDomain: "food-delivery-for-the-homeless.firebaseapp.com",
+	databaseURL: "https://food-delivery-for-the-homeless.firebaseio.com",
+	projectId: "food-delivery-for-the-homeless",
+	storageBucket: "food-delivery-for-the-homeless.appspot.com",
+	messagingSenderId: "1048473239710",
+	appId: "1:1048473239710:web:5723f204a0e5a2fef27b8f",
+	measurementId: "G-BWWYR6Y856"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
+const db = firebase.firestore();
 
-	// Add a second document with a generated ID.
-	// db.collection("users").add({
-	// 	first: "Alan",
-	// 	middle: "Mathison",
-	// 	last: "Tsafd",
-	// 	born: 1912
-	// })
-	// 	.then(({id}) => {
-	// 		console.log("Document written with ID: ", id);
-	// 	})
-	// 	.catch(error => {
-	// 		console.error("Error adding document: ", error);
-	// 	});
-});
+// Add a second document with a generated ID.
+// db.collection("users").add({
+// 	first: "Alan",
+// 	middle: "Mathison",
+// 	last: "Tsafd",
+// 	born: 1912
+// })
+// 	.then(({id}) => {
+// 		console.log("Document written with ID: ", id);
+// 	})
+// 	.catch(error => {
+// 		console.error("Error adding document: ", error);
+// 	});
--- files/login_handler.js
+++ files/login_handler.js
@@ -1,5 +1,10 @@
-function userLogin(email, password)
-{
+const $loginForm = document.querySelector("#login")
+$loginForm.onsubmit = e => {
+	console.log(e)
+	e.preventDefault()
+}
+
+function userLogin(email, password){
 	firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
 		alert('login failed');
 	});
