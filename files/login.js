650c957f9a4656d43df29554feb3d8f431582525
Darryl Yeo <dev@darryl-yeo.com>
2020-01-11 13:39:02 -0800

    Remove duplicate code

--- files/login.js
+++ files/login.js
@@ -8,48 +8,6 @@ window.onclick = function(event) {
   }
 }
 
-function setLocation() {
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(({coords}) => {
-			let pos = {
-				lat: coords.latitude,
-				lng: coords.longitude
-			};
-			map.setCenter(pos);
-			map.setZoom(13);
-			let currentInfo = new google.maps.InfoWindow({
-				content: "This is your current location!"
-			});
-			let image = {
-				url: './images/current_loc.png',
-				scaledSize: new google.maps.Size(50, 50),// scaled size maintaing aspect ratio
-				origin: new google.maps.Point(0, 0), // origin
-				anchor: new google.maps.Point(50, 50) // anchor
-			};
-			let currentLoc = new google.maps.Marker({
-				position: map.getCenter(),
-				icon: image,
-				animation: google.maps.Animation.BOUNCE,
-				map
-			});
-			setTimeout(() => { currentLoc.setAnimation(null) }, 3000);
-			currentLoc.addListener('mouseover', function () {
-				currentInfo.open(map, this);
-			});
-
-			// assuming you also want to hide the infowindow when user mouses-out
-			currentLoc.addListener('mouseout', () => {
-				currentInfo.close();
-			});
-		}, () => {
-			handleLocationError(true, infoWindow, map.getCenter());
-		});
-	} else {
-		// Browser doesn't support Geolocation
-		handleLocationError(false, infoWindow, map.getCenter());
-	}
-}
-
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 	window.alert("Your browser doesn't support location access from google map!");
 }
