2b457c961cd5219eebaa45ff7fc49461e01b29d0
Darryl Yeo <dev@darryl-yeo.com>
2020-01-11 16:30:17 -0800

    Fix auth

--- files/index.js
+++ files/index.js
@@ -170,11 +170,6 @@ $loginForm.onsubmit = e => {
 	// Get form values
 	const {username, password} = Object.fromEntries(new FormData(e.target).entries())
 	
-	userLogin(username, password)
-}
-
-function userLogin(email, password){
-	firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-		alert('login failed');
-	});
+	firebase.auth().signInWithEmailAndPassword(email, password)
+		.catch(e => console.error(e));
 }
