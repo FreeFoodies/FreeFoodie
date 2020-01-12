5600b37235d191558c8bf4944dcd456ec58db32b
Darryl Yeo <dev@darryl-yeo.com>
2020-01-11 22:06:35 -0800

    Rearrange JS

--- files/index.js
+++ files/index.js
@@ -1,3 +1,18 @@
+// Initialize Firebase
+firebase.initializeApp({
+	apiKey: "AIzaSyAOI-pfdx43XfrMk9fBk0vfYH9kQzSOzP0",
+	authDomain: "food-delivery-for-the-homeless.firebaseapp.com",
+	databaseURL: "https://food-delivery-for-the-homeless.firebaseio.com",
+	projectId: "food-delivery-for-the-homeless",
+	storageBucket: "food-delivery-for-the-homeless.appspot.com",
+	messagingSenderId: "1048473239710",
+	appId: "1:1048473239710:web:5723f204a0e5a2fef27b8f",
+	measurementId: "G-BWWYR6Y856"
+})
+
+const db = firebase.firestore()
+
+
 // Buttons that toggle [hidden] state
 for(const $button of document.querySelectorAll('[data-toggle]')){
 	$button.onclick = e => {
@@ -113,23 +128,6 @@ function openSideModel(){
 	
 }
 
-// Firebase
-// jQuery methods go here...
-// Your web app's Firebase configuration
-const firebaseConfig = {
-	apiKey: "AIzaSyAOI-pfdx43XfrMk9fBk0vfYH9kQzSOzP0",
-	authDomain: "food-delivery-for-the-homeless.firebaseapp.com",
-	databaseURL: "https://food-delivery-for-the-homeless.firebaseio.com",
-	projectId: "food-delivery-for-the-homeless",
-	storageBucket: "food-delivery-for-the-homeless.appspot.com",
-	messagingSenderId: "1048473239710",
-	appId: "1:1048473239710:web:5723f204a0e5a2fef27b8f",
-	measurementId: "G-BWWYR6Y856"
-}
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig)
-
-const db = firebase.firestore()
 
 // Add a second document with a generated ID.
 // db.collection("users").add({
