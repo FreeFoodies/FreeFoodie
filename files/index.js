49217e871bea67b803527c32ba2483136dd657a7
Darryl Yeo <dev@darryl-yeo.com>
2020-01-11 16:54:49 -0800

    Move Add Food Location button

--- files/index.js
+++ files/index.js
@@ -1,83 +1,49 @@
-function CenterControl(controlDiv, map) {
-
-        // Set CSS for the control border.
-        var controlUI = document.createElement('div');
-        controlUI.style.backgroundColor = '#fff';
-        controlUI.style.border = '2px solid #fff';
-        controlUI.style.borderRadius = '3px';
-        controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
-        controlUI.style.cursor = 'pointer';
-        controlUI.style.marginBottom = '22px';
-        controlUI.style.textAlign = 'center';
-        controlUI.title = 'Click to recenter the map';
-        controlDiv.appendChild(controlUI);
-
-        // Set CSS for the control interior.
-        var controlText = document.createElement('div');
-        controlText.style.color = 'rgb(25,25,25)';
-        controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
-        controlText.style.fontSize = '16px';
-        controlText.style.lineHeight = '38px';
-        controlText.style.paddingLeft = '5px';
-        controlText.style.paddingRight = '5px';
-        controlText.innerHTML = 'Add Food Location';
-        controlUI.appendChild(controlText);
-
-        // Setup the click event listeners: simply set the map to Chicago.
-        controlUI.addEventListener('click', function() {
-          map.setOptions({draggableCursor:'crosshair'});
-          map.addListener('click',function(){
-          	alert("open model");
-          })
-        });
-
-      }
-
 function whenGoogleMapsAPIReady() {
 	const map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: 39.8283, lng: -98.5795},
 		zoom: 5
 	})
 
-	// Create the DIV to hold the control and call the CenterControl()
-    // constructor passing in this DIV.
-    var centerControlDiv = document.createElement('div');
-    var centerControl = new CenterControl(centerControlDiv, map);
+	const $addFoodLocationButton = $('.add-food-location')
+	$addFoodLocationButton.onclick = () => {
+		map.setOptions({draggableCursor: 'crosshair'});
+		map.addListener('click', () => {
+			console.log('Add food location')
+		})
+	}
 
-    centerControlDiv.index = 1;
-    map.controls[google.maps.ControlPosition.BOTTOM_LEFT].push(centerControlDiv);
+	// map.controls[google.maps.ControlPosition.BOTTOM_LEFT].push($addFoodLocationButton);
 
-    db.collection("Pins").
-    	onSnapshot(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
-        	console.log(doc.data().latitude);
-        	console.log(doc.data().longitude);
-			var infowindow = new google.maps.InfoWindow({
+	db.collection("Pins").onSnapshot((querySnapshot) => {
+		querySnapshot.forEach(function(doc) {
+			console.log(doc.data().latitude);
+			console.log(doc.data().longitude);
+			const infowindow = new google.maps.InfoWindow({
 				content: "Pin info"
 			});
 
-			var marker = new google.maps.Marker({
+			const marker = new google.maps.Marker({
 				position: {lat: doc.data().latitude, lng: doc.data().longitude},
 				map: map,
 				title: 'Test Pin'
 			});
-				marker.addListener('click', function() {
-				infowindow.open(map, marker);
+			marker.addListener('click', function() {
+			infowindow.open(map, marker);
 			});
-        });
-    });
+		});
+	});
 /*	map.addListener('click', function(e) {
-	  db.collection("Pins").doc("test").set({
-	    latitude: e.latLng.lat(),
-	    longitude: e.latLng.lng()
-	  })
-	  .then(function() {
-	      console.log("Document successfully written!");
-	  })
-	  .catch(function(error) {
-	      console.error("Error writing document: ", error);
-	  });
-  });*/
+	db.collection("Pins").doc("test").set({
+		latitude: e.latLng.lat(),
+		longitude: e.latLng.lng()
+	})
+	.then(function() {
+		console.log("Document successfully written!");
+	})
+	.catch(function(error) {
+		console.error("Error writing document: ", error);
+	});
+});*/
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(({coords}) => {
 			map.setCenter({
